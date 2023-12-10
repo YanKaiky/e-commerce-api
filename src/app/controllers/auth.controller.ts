9fcb8896404aa72ed2c2f4734112e871711ae814
Yan Kaiky <yankaikys@gmail.com>
2023-12-10 19:39:19 -0300

    feat(AuthController): add error

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -3,18 +3,22 @@ import AuthService from '../services/auth.service';
 
 class AuthController {
   public login = async (request: Request, response: Response) => {
-    const authorization = request.headers.authorization as string;
+    try {
+      const authorization = request.headers.authorization as string;
 
-    const auth = Buffer.from(authorization.split(' ')[1], 'base64').toString().split(':');
+      const auth = Buffer.from(authorization.split(' ')[1], 'base64').toString().split(':');
 
-    const payload = {
-      email: auth[0],
-      password: auth[1],
-    };
+      const payload = {
+        email: auth[0],
+        password: auth[1],
+      };
 
-    const data = await AuthService.login(payload);
+      const data = await AuthService.login(payload);
 
-    response.status(200).json(data);
+      response.status(200).json(data);
+    } catch (error) {
+      response.status(401).json(error);
+    }
   };
 }
 
