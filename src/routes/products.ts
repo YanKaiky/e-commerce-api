d35f6482ea5fb6318064ebe08684693dd0a1d7da
Yan Kaiky <yankaikys@gmail.com>
2023-12-10 04:27:28 -0300

    feat(routes): remove auth to products

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,17 +1,16 @@
 import express from 'express';
 import ProductsController from '../app/controllers/products.controller';
-import auth from '../app/middleware/auth.middleware';
 
 const router = express.Router();
 
-router.post('/', auth, ProductsController.create);
+router.post('/', ProductsController.create);
 
-router.get('/', auth, ProductsController.getAll);
+router.get('/', ProductsController.getAll);
 
-router.get('/:id', auth, ProductsController.getById);
+router.get('/:id', ProductsController.getById);
 
-router.put('/:id', auth, ProductsController.update);
+router.put('/:id', ProductsController.update);
 
-router.delete('/:id', auth, ProductsController.delete);
+router.delete('/:id', ProductsController.delete);
 
 export default router;
